04200acb289e247ab80f4d523aee5794f5bf5161
Andreas Hendriksen <prak_ahe@ao.dk>
2025-01-29 15:01:30 +0100

    asdasd

--- fastapi-sample-app/app/main.py
+++ fastapi-sample-app/app/main.py
@@ -1,3 +1,5 @@
+import datetime
+
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
@@ -6,15 +8,15 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, FastAPI!"}
+    return {"message": "Hello, FastAPI!", "timecode": datetime.datetime.now()}
 
 
 @app.get("/web", response_class=HTMLResponse)
 def web_page():
-    return "<h1>Welcome to FastAPI</h1><p>This is a simple web page.</p>"
+    return "<h1>Welcome to FastAPI</h1><p>This is a simple web page.</p><p>Hest!</p>"
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=18000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
