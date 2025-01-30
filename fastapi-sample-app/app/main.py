869a3a43fa5c0f97703582b6cab71b31b35534a3
Andreas Hendriksen <prak_ahe@ao.dk>
2025-01-30 14:33:23 +0100

    Made crash endpoint actually creash and added error endpoint.

--- fastapi-sample-app/app/main.py
+++ fastapi-sample-app/app/main.py
@@ -14,6 +14,7 @@ app = FastAPI()
 ENV_MESSAGE = os.getenv("MESSAGE", "Default message from FastAPI")
 
 crash_on_call = -1
+error_on_call = -1
 
 
 if __name__ == "__main__":
@@ -30,6 +31,7 @@ def startup():
 @app.get("/", response_class=FileResponse)
 def web_page():
     check_crash()
+    check_error()
     file_path = Path(__file__).parent / "static" / "index.html"
     return FileResponse(file_path)
 
@@ -42,6 +44,7 @@ def healthcheck():
 @app.get("/env")
 def read_env():
     check_crash()
+    check_error()
     return {"message": ENV_MESSAGE, "info": info_object()}
 
 
@@ -49,6 +52,7 @@ def read_env():
 def save_message(text: str = Form(...), db: Session = Depends(get_db)):
     """Save a message to the database."""
     check_crash()
+    check_error()
     new_message = Message(text=text)
     db.add(new_message)
     db.commit()
@@ -60,13 +64,22 @@ def save_message(text: str = Form(...), db: Session = Depends(get_db)):
 def get_messages(db: Session = Depends(get_db)):
     """Retrieve all messages from the database."""
     check_crash()
+    check_error()
     messages = db.query(Message).all()
     return [{"id": m.id, "text": m.text, "timestamp": m.timestamp} for m in messages]
 
 
+@app.get("/code/{code}")
+def return_code(code: int):
+    check_crash()
+    check_error()
+    return Response(status_code=code)
+
+
 @app.get("/crash/")
 def crash_now():
-    raise RuntimeError("Intentional crash!")
+    print("Intentional crash!")
+    os._exit(1)
 
 
 @app.get("/crash/{amt}")
@@ -76,11 +89,33 @@ def crash_on_n_call(amt: int):
     return {"message": f"Container will crash during request number {amt} after this."}
 
 
+@app.get("/error/")
+def error_now():
+    raise RuntimeError("Intentional error!")
+
+
+@app.get("/error/{amt}")
+def error_on_n_call(amt: int):
+    global error_on_call
+    error_on_call = amt
+    return {
+        "message": f"Container will raise a RuntimeError during request number {amt} after this."
+    }
+
+
 def check_crash():
     global crash_on_call
     crash_on_call = crash_on_call - 1
     if crash_on_call == 0:
-        raise RuntimeError("Intentional crash!")
+        print("Intentional crash!")
+        os._exit(1)
+
+
+def check_error():
+    global error_on_call
+    error_on_call = error_on_call - 1
+    if error_on_call == 0:
+        raise RuntimeError("Intentional error!")
 
 
 def info_object():
